bf4ef814a29c5181777ad6f77d26a3acc71f53a8
Boris Grigorov <boris.grigorov26@gmail.com>
2020-12-30 12:34:15 +0100

    Added more game modes

--- discord.js
+++ discord.js
@@ -10,15 +10,15 @@ module.exports = class Discord {
     this.client = new DiscordRPC.Client({ transport: "ipc" });
 
     this.client.on("ready", () => {
-      if(config.richOutput) console.log("Connected to discord");
+      if (config.richOutput) console.log("Connected to discord");
     });
     var clientId = this.clientId;
     await this.client.login({ clientId });
     this.loggedIn = true;
   }
 
-  async clearActivity(pid){
-    if(config.richOutput) console.log("Activity cleared");
+  async clearActivity(pid) {
+    if (config.richOutput) console.log("Activity cleared");
     await this.client.clearActivity(pid);
   }
 
@@ -31,8 +31,11 @@ module.exports = class Discord {
       activity.largeImageKey = "menu";
       activity.largeImageText = "Main Menu";
     } else if (data.activity == "playing") {
-      activity.state = this.getTeam(data.game.player.team);
-      activity.details = this.getGameMode(data.game.map.mode) + " " + this.getCurrentPoints(data.game);
+      activity.state = this.getTeam(data.game);
+      activity.details =
+        this.getGamePhase(data.game) +
+        " " +
+        this.getCurrentPoints(data.game);
       activity.largeImageKey = data.game.map.name;
       activity.largeImageText = data.game.map.name;
       activity.smallImageKey = data.game.map.mode;
@@ -49,24 +52,53 @@ module.exports = class Discord {
         return "Casual";
       case "competitive":
         return "Competitive";
-      case "deatchmatch":
-        return "Deatchmatch";
-      case "demolition":
+      case "deathmatch":
+        return "Deathmatch";
+      case "gungametrbomb":
         return "Demolition";
-      case "demolitionshield":
-        return "Demolition";
+      case "flyingscoutsman":
+        return "Flying Scoutsman";
+      case "Overwatch":
+        return "Overwatch";
+      case "survival":
+        return "Danger zone";
+      case "gungameprogressive":
+        return "Arms race";
+      case "training":
+        return "Training";
+      default:
+        return "Unknown game type";
     }
   }
 
-  getTeam(team) {
-    if (team == "CT") {
+  getTeam(data) {
+    if (data.map.mode == "survival") {
+      return "";
+    } else if (data.player.team == "CT") {
       return "Team Counter-Terrorists";
-    } else if (team == "T") {
+    } else if (data.player.team == "T") {
       return "Team Terrorists";
     }
   }
 
-  getCurrentPoints(game){
-    return "(CT: " + game.map.team_ct.score + ", T:" + game.map.team_t.score + ")";
+  getCurrentPoints(game) {
+    return (
+      "(CT: " + game.map.team_ct.score + ", T:" + game.map.team_t.score + ")"
+    );
+  }
+
+  getGamePhase(data) {
+    switch (data.map.phase) {
+      case "warmup":
+        return "Warmup";
+      case "live":
+        return "In-Game";
+      case "intermission":
+        return "Half-Time";
+      case "gameover":
+        return "Game Over";
+      default:
+        return data.map.phase;
+    }
   }
 };
